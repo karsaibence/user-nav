590846fec225bede19c542609a7acbfd834e8d32
karsaibence <karesz333@hotmail.com>
2025-02-03 17:38:09 +0100

    docker indító farigcsálás

--- docker_start.bat
+++ docker_start.bat
@@ -1,5 +1,11 @@
 @echo off
 
+echo react build starting...
+cd ./frontend
+call npm run build
+
+cd ..
+echo react build finished...
 REM Az alapértelmezett Docker Compose parancsok futtatása
 echo Docker Compose build and up process is starting...
 
